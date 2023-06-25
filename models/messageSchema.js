85652398702cffb7c0bb5e26bd646e83946385c9
rubeenapk <rubeenaanwar5354@gmail.com>
2023-06-25 18:22:01 +0530

    message schema updated

--- models/messageSchema.js
+++ models/messageSchema.js
@@ -1,7 +1,24 @@
 const mongoose=require ('mongoose')
 
 const messageSchema= new mongoose.Schema({
-
+        chat_room_id: {
+          type: Schema.Types.ObjectId,
+          ref: 'ChatRoom',
+          required: true
+        },
+        sender_id: {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+          required: true
+        },
+        content: {
+          type: String,
+          required: true
+        },
+        timestamp: {
+          type: Date,
+          default: Date.now
+        }
 })
 
 module.exports=mongoose.model('Message',messageSchema)
